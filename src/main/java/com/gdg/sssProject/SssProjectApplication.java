99e122dfdecd55278d4da23b25b269191bf35a8e
jihoo2002 <yolohoo@naver.com>
2025-02-07 12:45:14 +0900

    add 초기 환경 세팅

---
+++ src/main/java/com/gdg/sssProject/SssProjectApplication.java
@@ -0,0 +1,13 @@
+package com.gdg.sssProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SssProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SssProjectApplication.class, args);
+	}
+
+}
