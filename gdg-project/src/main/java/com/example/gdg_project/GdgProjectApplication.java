fd4c98f4d67f2968ca328ecd303156378a88797d
dlgkrud <166580018+dlgkrud@users.noreply.github.com>
2025-02-16 02:45:20 +0900

    이슈 #4: 신고 정보 API 구현

---
+++ gdg-project/src/main/java/com/example/gdg_project/GdgProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.gdg_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GdgProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GdgProjectApplication.class, args);
+	}
+
+}
---
+++ gdg-project/src/test/java/com/example/gdg_project/GdgProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gdg_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GdgProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
