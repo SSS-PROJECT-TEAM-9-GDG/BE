f008d81a855832295ac6e0f7ea0311b1d26b7156
jihoo2002 <yolohoo@naver.com>
2025-02-27 01:21:21 +0900

    fix cors 설정

--- src/main/java/com/gdg/sssProject/global/WebConfig.java
+++ src/main/java/com/gdg/sssProject/global/WebConfig.java
@@ -11,16 +11,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*")
+        registry.addMapping("/api/noise/**")  // /api/noise/ 경로만 허용
+                .allowedOriginPatterns("*")  // 모든 출처 허용
                 .allowedMethods(
                         HttpMethod.GET.name(),
                         HttpMethod.HEAD.name(),
                         HttpMethod.POST.name(),
                         HttpMethod.PUT.name(),
                         HttpMethod.PATCH.name(),
-                        HttpMethod.DELETE.name());
+                        HttpMethod.DELETE.name())
+                .allowedHeaders("*")  // 모든 헤더 허용
+                .maxAge(3600);  // Preflight 요청 캐시 시간 설정 (1시간)
     }
-
 }
 
