3f04cedce7b84ada233a7b34db37cc93d6b7d3d3
jihoo2002 <yolohoo@naver.com>
2025-02-27 01:26:29 +0900

    fix webConfig

--- src/main/java/com/gdg/sssProject/global/WebConfig.java
+++ src/main/java/com/gdg/sssProject/global/WebConfig.java
@@ -1,27 +1,27 @@
-package com.gdg.sssProject.global;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-
-@Configuration
-public class WebConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/noise/**")  // /api/noise/ 경로만 허용
-                .allowedOriginPatterns("*")  // 모든 출처 허용
-                .allowedMethods(
-                        HttpMethod.GET.name(),
-                        HttpMethod.HEAD.name(),
-                        HttpMethod.POST.name(),
-                        HttpMethod.PUT.name(),
-                        HttpMethod.PATCH.name(),
-                        HttpMethod.DELETE.name())
-                .allowedHeaders("*")  // 모든 헤더 허용
-                .maxAge(3600);  // Preflight 요청 캐시 시간 설정 (1시간)
-    }
-}
-
+//package com.gdg.sssProject.global;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.http.HttpMethod;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//
+//@Configuration
+//public class WebConfig implements WebMvcConfigurer {
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/api/noise/**")  // /api/noise/ 경로만 허용
+//                .allowedOriginPatterns("*")  // 모든 출처 허용
+//                .allowedMethods(
+//                        HttpMethod.GET.name(),
+//                        HttpMethod.HEAD.name(),
+//                        HttpMethod.POST.name(),
+//                        HttpMethod.PUT.name(),
+//                        HttpMethod.PATCH.name(),
+//                        HttpMethod.DELETE.name())
+//                .allowedHeaders("*")  // 모든 헤더 허용
+//                .maxAge(3600);  // Preflight 요청 캐시 시간 설정 (1시간)
+//    }
+//}
+//
